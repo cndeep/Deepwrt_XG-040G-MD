238d1cae1883c8e5d89723f61054a03c32f26262
cndeep <145043517+cndeep@users.noreply.github.com>
2026-04-15 13:08:17 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -10,6 +10,10 @@ git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-per
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-
-
+# Modify version
+sed -i 's/ImmortalWrt/DeepWrt/g' package/base-files/image-config.in
+sed -i 's/ImmortalWrt/DeepWrt/g' include/version.mk
+sed -i 's/ImmortalWrt/DeepWrt/g' package/base-files/files/bin/config_generate
